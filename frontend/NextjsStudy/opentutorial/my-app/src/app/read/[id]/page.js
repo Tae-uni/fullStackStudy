96ddffa19cb4d0887d707ac30b23b91ef449f46e
taekwon <universe134679@gmail.com>
2024-05-31 22:43:54 +0900

    read/update pages with JSON server

--- frontend/NextjsStudy/opentutorial/my-app/src/app/read/[id]/page.js
+++ frontend/NextjsStudy/opentutorial/my-app/src/app/read/[id]/page.js
@@ -1,5 +1,5 @@
 export default async function Read(props) {
-    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`, {cache:'no-store'});
     const topic = await resp.json();
     return (
         <>
---
+++ frontend/NextjsStudy/opentutorial/my-app/src/app/update/[id]/page.js
@@ -0,0 +1,55 @@
+"use client" // client side Compnent
+
+// import { useRouter } from "next/router"; // next version 12 router..
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Update() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const router = useRouter(); // Redirect after create the group or client side route..
+    const params = useParams();
+    const id = params.id;
+    useEffect(() => {
+        fetch('http://localhost:9999/topics/' + id)
+            .then(resp => resp.json())
+            .then(result => {
+                setTitle(result.title);
+                setBody(result.body);
+            });
+    }, []);
+    return (
+        <form onSubmit={(e) => {
+            e.preventDefault(); // prevent 
+            const title = e.target.title.value;
+            const body = e.target.body.value;
+            const options = {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title, body })
+            }
+            fetch(`http://localhost:9999/topics/`+id, options)
+                .then((res) => res.json())
+                .then((result) => {
+                    console.log(result);
+                    const lastid = result.id;
+                    router.push(`/read/${lastid}`);
+                    router.refresh();
+                })
+        }}>
+            <p>
+                <input type="text" name="title" placeholder="title" value={title}
+                    onChange={e => setTitle(e.target.value)} />
+            </p>
+            <p>
+                <textarea name="body" placeholder="body" value={body}
+                    onChange={e => setBody(e.target.value)}></textarea>
+            </p>
+            <p>
+                <input type="submit" value="update" />
+            </p>
+        </form>
+    )
+}
