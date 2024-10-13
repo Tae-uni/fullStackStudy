3fa94b0420711f433536305e3b0b1328de5079fa
taekwon <universe134679@gmail.com>
2024-10-13 13:56:14 +0900

    :sparkles: Cookies -add cookie/handling cookie with signed option using cookie-parser

--- backend/express-practice/src/index.mjs
+++ backend/express-practice/src/index.mjs
@@ -1,11 +1,14 @@
 import express from 'express';
 import routes from "./routes/index.mjs";
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
 
-// Router
+// cookieParser uses a `Secret Key` to enable the signed option for cookies.
+// It is crucial for preventing clients from tampering with cookies. Normally, an array of keys is set in `.env` files.
+app.use(cookieParser('helloValue'));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -15,12 +18,8 @@ app.listen(PORT, () => {
 });
 
 // Root router handler
-app.get("/",
-  (request, response, next) => {
-    console.log("Base URL");
-    next();
-  },
-  (request, response) => {
-    // response.send("Hello, World!");
-    response.status(201).send({ msg: 'HI' });
-  });
+app.get("/", (request, response) => {
+  response.cookie("hello", "value", { maxAge: 10000, signed: true });
+  // response.send("Hello, World!");
+  response.status(201).send({ msg: 'HI' });
+});
--- backend/express-practice/src/routes/products.mjs
+++ backend/express-practice/src/routes/products.mjs
@@ -3,9 +3,16 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/api/products', (request, response) => {
-  response.send([
-    { id: 123, name: "Chicken breast", price: 12.99 }
-  ]);
+  console.log(request.headers.cookie);
+  console.log(request.cookies);
+  console.log(request.signedCookies.hello);
+  if (request.signedCookies.hello && request.signedCookies.hello === 'value') {
+    response.send([
+      { id: 123, name: "Chicken breast", price: 12.99 }
+    ]);
+  } else {
+    return response.status(403).send({ msg: "Sorry. You need the correct cookie" });
+  }
 });
 
 export default router;
